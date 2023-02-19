758001299dfb59cf4625e4f1276cfa7cb8de15c0
SamsonKalume <samson.kalume@student.moringaschool.com>
2023-02-19 22:53:49 +0300

    Modified configs db settings

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://samsonkalume:S@ltwaterF!sh@cluster0.igjkog.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://samsonkalume:S@ltwaterF!sh@cluster0.igjkog.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://samsonkalume:S@ltwaterF!sh@cluster0.igjkog.mongodb.net/?retryWrites=true&w=majority',
+    production: 'mongodb+srv://samsonkalume:S@ltwaterF!sh@cluster0.igojkog.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://samsonkalume:S@ltwaterF!sh@cluster0.igojkog.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://samsonkalume:S@ltwaterF!sh@cluster0.igojkog.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
