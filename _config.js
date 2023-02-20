1e072b4be3757ce3dfa92bdecf51ff731b9f8082
SamsonKalume <samson.kalume@student.moringaschool.com>
2023-02-20 18:03:31 +0300

    Changed Configs

--- _config.js
+++ _config.js
@@ -3,6 +3,7 @@ var config = {}
 // Update to have your correct username and password
 config.mongoURI = {
     test: 'mongodb+srv://samsonkalume:S@ltwaterF!sh@cluster0.igojkog.mongodb.net/?retryWrites=true&w=majority',
+    production: 'mongodb+srv://samsonkalume:S@ltwaterF!sh@cluster0.igojkog.mongodb.net/?retryWrites=true&w=majority',
     //production: 'mongodb+srv://samsonkalume:S@ltwaterF!sh@cluster0.igojkog.mongodb.net/darkroom?retryWrites=true&w=majority',
     //development: 'mongodb+srv://samsonkalume:S@ltwaterF!sh@cluster0.igojkog.mongodb.net/darkroom?retryWrites=true&w=majority',
     //test: 'mongodb+srv://samsonkalume:S@ltwaterF!sh@cluster0.igojkog.mongodb.net/darkroom?retryWrites=true&w=majority',
