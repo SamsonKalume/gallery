94a5546014bcc4a2fb838533b1c3559a0b055842
SamsonKalume <samson.kalume@student.moringaschool.com>
2023-02-20 20:09:26 +0300

    Edited Config Settings with MongoDB settings

--- _config.js
+++ _config.js
@@ -2,9 +2,9 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    test: 'mongodb+srv://samsonkalume:S@ltwaterF!sh@cluster0.igojkog.mongodb.net/?retryWrites=true&w=majority',
-    production: 'mongodb+srv://samsonkalume:S@ltwaterF!sh@cluster0.igojkog.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://samsonkalume:S@ltwaterF!sh@cluster0.igojkog.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://samsonkalume:Sa@ka@420N@moringa.kegv4xm.mongodb.net/?retryWrites=true&w=majority',
+    production: 'mongodb+srv://samsonkalume:Sa@ka@420N@moringa.kegv4xm.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://samsonkalume:Sa@ka@420N@moringa.kegv4xm.mongodb.net/?retryWrites=true&w=majority',
     //production: 'mongodb+srv://samsonkalume:S@ltwaterF!sh@cluster0.igojkog.mongodb.net/darkroom?retryWrites=true&w=majority',
     //development: 'mongodb+srv://samsonkalume:S@ltwaterF!sh@cluster0.igojkog.mongodb.net/darkroom?retryWrites=true&w=majority',
     //test: 'mongodb+srv://samsonkalume:S@ltwaterF!sh@cluster0.igojkog.mongodb.net/darkroom?retryWrites=true&w=majority',
